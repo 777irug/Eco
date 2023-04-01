8d8b43280835538f05879ccda031f09b67f207d5
777irug <bboi151079@gmail.com>
2023-04-01 21:36:27 +0530

    next-update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4f09614d319b8979"; }
+/******/ 		__webpack_require__.h = function() { return "595e8e1b65c8f938"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
